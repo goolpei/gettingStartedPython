2bbeb02828d9c77ee6e21d94adbe26daa129bccf
Matthew Venice Balaod <matthewvenicebalaod@gmail.com>
2026-08-11 11:06:22 +0800

    DSA study

--- DSA/Algorithms/DijkstraAlgorithm.py
+++ DSA/Algorithms/DijkstraAlgorithm.py
@@ -1,10 +1,55 @@
 import heapq
 
-def find_shortest_path(adj_matrix, start_node, target_node = None):
-    INF = float('inf')
-    n = len(adj_matrix)
+def find_shortest_path(graph, start_node, target_node = None):
 
-    distances = [INF] * n
+    # graph is an adjacency list representation
+
+    INF = float('inf')
+    n = len(graph)
+
+    distances = {node: INF for node in graph}
     distances[start_node] = 0
-    previous = [None] * n
+
+    previous_nodes = {node: None for node in graph}
     visited = set()
+
+    min_heap = [(0, start_node)]
+
+    while min_heap:
+        current_distance, current_node = heapq.heappop(min_heap)
+        if current_node in visited:
+            continue
+        visited.add(current_node)
+        if current_node == target_node:
+            break
+
+        for neighbor, weight in graph[current_node]:
+            if neighbor in visited:
+                continue
+            new_distance = current_distance + weight
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
+                previous_nodes[neighbor] = current_node
+                heapq.heappush(min_heap, (new_distance, neighbor))
+
+    path = []
+    if target_node is not None:
+        curr = target_node
+        if distances[target_node] != INF:
+            while curr is not None:
+                path.append(curr)
+                curr = previous_nodes[curr]
+            path.reverse()
+        else:
+            path = None
+
+    return distances, path
+
+test_graph = {
+    'A': [('B', 2), ('C', 5)],
+    'B': [('A', 2), ('D', 1)],
+    'C': [('A', 5), ('D', 1)],
+    'D': [('B', 1), ('C', 1)]
+}
+
+print(find_shortest_path(test_graph, 'A'))
--- DSA/Algorithms/N_Queens_algorithm.py
+++ DSA/Algorithms/N_Queens_algorithm.py
@@ -2,18 +2,85 @@
 # stack
 def dfs_n_queens(n):
     # should return a list of solutions
+
     if n < 1: return []
+    
+    col = set()
+    pos_dia = set()
+    neg_dia = set()
 
-    # we need a set of column, diagonal, and anti-diagonal to indicate that it is taken / occupied by a queen's range
-    # no need to indicate row since 
-
-    occupied_col = set()
-    occupied_dia = set() # dia and anti-dia
+    def square_is_clear(row, column):
+        return False if (column in col) or (row + column in pos_dia) or (row - column in neg_dia) else True
 
     solutions = []
+    board_solutions = []
+    board = [['.'] * n for _ in range(n)]
+
+    r, c = 0, 0
     stack = []
 
-    while stack:
+    while True:
+
+
+        found_valid_col = False
+        while c < n:
+            if square_is_clear(r, c):
+                found_valid_col = True
+                break
+            c += 1
+
+        if found_valid_col:
+            stack.append((r, c))
+            board[r][c] = 'Q'
+
+            col.add(c)
+            pos_dia.add(r + c)
+            neg_dia.add(r - c)
+
+            if len(stack) == n:
+                sol = [x for _, x in stack]
+                solutions.append(sol)
+                board_solutions.append([row[:] for row in board])
+
+                last_r, last_c = stack.pop()
+                board[last_r][last_c] = '.'
+
+                col.remove(last_c)
+                pos_dia.remove(last_r + last_c)
+                neg_dia.remove(last_r - last_c)
+
+                r = last_r
+                c = last_c + 1
+            else:
+                r += 1
+                c = 0
+
+        else:
+            if not stack:
+                break
+
+            prev_r, prev_c = stack.pop()
+            board[prev_r][prev_c] = '.'
+            col.remove(prev_c)
+            pos_dia.remove(prev_r + prev_c)
+            neg_dia.remove(prev_r - prev_c)
+
+            r = prev_r
+            c = prev_c + 1
+
+
+    # for b in board_solutions:
+    #     for row in b:
+    #         print(' '.join(row))
+    #     print('\n')
+
+
+    return solutions
+
         
 
+
+    
+
+dfs_n_queens(4)
 
